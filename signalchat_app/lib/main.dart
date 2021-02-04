934f85aa2518830529cdf1aeea8cb1ee28bc234f
Syed Jafor <sjhnadeem@gmail.com>
2021-02-04 10:49:27 +0600

    First commit for Signal Chat App

---
+++ signalchat_app/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Signal Chat App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: Center(
+          child: Text("Signal"),
+        ));
+  }
+}
